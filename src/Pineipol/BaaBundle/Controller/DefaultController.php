4a617f41348b926caed8b0f191aed6a1296c198f
Pineipol <pineipol@pineipol.com>
2015-03-15 21:53:29 +0100

    Layouting completed

--- src/Pineipol/BaaBundle/Controller/DefaultController.php
+++ src/Pineipol/BaaBundle/Controller/DefaultController.php
@@ -9,4 +9,16 @@ class DefaultController extends Controller {
     public function indexAction() {
         return $this->render('PineipolBaaBundle:Default:index.html.twig');
     }
+
+    public function rightColumnAction() {
+        return $this->render('PineipolBaaBundle:Default:right-column.html.twig');
+    }
+
+    public function contactAction() {
+        return $this->render('PineipolBaaBundle:Default:contact.html.twig');
+    }
+
+    public function listAction() {
+        return $this->render('PineipolBaaBundle:Default:list.html.twig');
+    }
 }
