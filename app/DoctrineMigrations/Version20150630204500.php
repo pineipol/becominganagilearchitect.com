272223dfd4e6db0d578b62a6ed22dfa5f7248c8e
Pineipol <pineipol@pineipol.com>
2015-06-30 21:05:50 +0200

    Headings code refactor and SQL migration completed

--- app/DoctrineMigrations/Version20150630204500.php
+++ app/DoctrineMigrations/Version20150630204500.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20150629230700 extends AbstractMigration {
+class Version20150630204500 extends AbstractMigration {
 
     /**
      * @param Schema $schema
