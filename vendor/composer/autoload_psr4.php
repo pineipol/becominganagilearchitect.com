d71f71a2ef0046159c5df754af04cbcb74c6c108
Pineipol <pineipol@pineipol.com>
2015-07-26 21:20:31 +0200

    Merge contact-form-page
    Conflicts:
    	src/Pineipol/BaaBundle/Resources/public/css/global.css
    	src/Pineipol/BaaBundle/Resources/public/css/global.css.map
    	src/Pineipol/BaaBundle/Resources/public/css/layout.css
    	src/Pineipol/BaaBundle/Resources/public/css/layout.css.map
    	src/Pineipol/BaaBundle/Resources/public/css/styles.css
    	src/Pineipol/BaaBundle/Resources/public/css/styles.css.map
    	src/Pineipol/BaaBundle/Resources/public/less/global.less

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -12,6 +12,8 @@ return array(
     'Sensio\\Bundle\\FrameworkExtraBundle\\' => array($vendorDir . '/sensio/framework-extra-bundle'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
     'Incenteev\\ParameterHandler\\' => array($vendorDir . '/incenteev/composer-parameter-handler'),
+    'Gregwar\\Captcha\\' => array($vendorDir . '/gregwar/captcha'),
+    'Gregwar\\CaptchaBundle\\' => array($vendorDir . '/gregwar/captcha-bundle'),
     'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
     'Doctrine\\Bundle\\DoctrineBundle\\' => array($vendorDir . '/doctrine/doctrine-bundle'),
 );
